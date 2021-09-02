684ed9644cc49cb0df621d3478cab93162a1fd1d
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2021-09-02 13:24:11 +0100

    setUp complete for card_game_tests.py

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -2,5 +2,12 @@ import unittest
 from src.card import Card
 from src.card_game import CardGame
 
-class TestCardGame:
+class TestCardGame(unittest.TestCase):
+    def setUp(self):
+        self.card1 = Card("Spades",9)
+        self.card2 = Card("Clubs",5)
+        self.card3 = Card("Hearts", 1)
+
+
+    def test_check_for_ace(self)
     pass
